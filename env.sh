e2d72cc787965f4daceb0c4c56e645fa4c608b05
Leandro Heck <leoheck@gmail.com>
2021-12-29 11:49:40 -0300

    Add sample script to set environment

---
+++ env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This is sample script to setup the environment.
+# Change the script changing the TOOLCHAIN_PATH accordingly
+
+TOOLCHAIN_PATH="./riscv32-unknown-elf/gcc-9.3.0/"
+
+export LANG="en_GB.UTF-8"
+
+# Pre-append path (binaries prefixed with arch name)
+export PATH="${TOOLCHAIN_PATH}/bin:${PATH}"
+
+# Pre-append path (binaries without prefix overlapping system settings) 
+export PATH="${TOOLCHAIN_PATH}/riscv32-unknown-elf/bin:${PATH}"
+
+# Headers
+export C_INCLUDE_PATH="${TOOLCHAIN_PATH}/include:${C_INCLUDE_PATH}"
+export CPLUS_INCLUDE_PATH="${TOOLCHAIN_PATH}/include:${CPLUS_INCLUDE_PATH}"
+
+# Libraries
+export LIBRARY_PATH="${TOOLCHAIN_PATH}/lib:${LIBRARY_PATH}"
+
+# Documentation
+prepend-path MANPATH "${BASE}/share/man"
+prepend-path INFOPATH "${BASE}/share/info"
