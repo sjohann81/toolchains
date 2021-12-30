37675df7fcfd4a73f3fd9c26ff2ee927e441a4f5
Leandro Heck <leoheck@gmail.com>
2021-12-30 11:02:05 -0300

    Fix commands and improve quotes

--- env.sh
+++ env.sh
@@ -8,18 +8,18 @@ TOOLCHAIN_PATH="./riscv32-unknown-elf/gcc-9.3.0/"
 export LANG="en_GB.UTF-8"
 
 # Pre-append path (binaries prefixed with arch name)
-export PATH="${TOOLCHAIN_PATH}/bin:${PATH}"
+export PATH="${TOOLCHAIN_PATH}/bin":${PATH}
 
-# Pre-append path (binaries without prefix overlapping system settings) 
-export PATH="${TOOLCHAIN_PATH}/riscv32-unknown-elf/bin:${PATH}"
+# Pre-append path (binaries without prefix overlapping system settings)
+export PATH="${TOOLCHAIN_PATH}/riscv32-unknown-elf/bin":${PATH}
 
 # Headers
-export C_INCLUDE_PATH="${TOOLCHAIN_PATH}/include:${C_INCLUDE_PATH}"
-export CPLUS_INCLUDE_PATH="${TOOLCHAIN_PATH}/include:${CPLUS_INCLUDE_PATH}"
+export C_INCLUDE_PATH="${TOOLCHAIN_PATH}/include":${C_INCLUDE_PATH}
+export CPLUS_INCLUDE_PATH="${TOOLCHAIN_PATH}/include":${CPLUS_INCLUDE_PATH}
 
 # Libraries
-export LIBRARY_PATH="${TOOLCHAIN_PATH}/lib:${LIBRARY_PATH}"
+export LIBRARY_PATH="${TOOLCHAIN_PATH}/lib":${LIBRARY_PATH}
 
 # Documentation
-prepend-path MANPATH "${BASE}/share/man"
-prepend-path INFOPATH "${BASE}/share/info"
+export MANPATH="${BASE}/share/man":${MANPATH}
+export INFOPATH="${BASE}/share/info":${INFOPATH}
